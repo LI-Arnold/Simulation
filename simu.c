228d73ab9aeb5061d114fe90275e92f88060379b
shadealao <alao.a.s@gmail.com>
2020-05-21 20:48:52 +0200

    fonction init_anneau

--- simu.c
+++ simu.c
@@ -5,8 +5,11 @@
 #include <stdlib.h>
 
 #define MAXEVENT 10000
-
+#define nbSlot 150
+#define K 10 //nbr de stations dans l'anneau
 int N; // total conteneurs qui ont été produit
+int temps;
+int Nservi; // conteneurs sortis du système
 
 
 typedef struct Event{
@@ -22,6 +25,28 @@ typedef struct Echeancier{
 
 echeancier E;
 
+typedef struct Conteneur {
+	int position;
+	int nbdeplacement; // nombre de déplacement à effectuer avant de sortir de l'anneau
+}conteneur;
+
+typedef struct Slot {
+	int occupe; // 0 quand c'est libre et 1 quand c'est occupe
+	int numero; //numero de chaque slot
+}slot;
+
+typedef struct Station {
+	int position;
+	int Nstation; //nb de contenaur que la station possede
+	int delta;
+}station;
+
+typedef struct Anneau {
+	station Stati[K];
+	slot Slo[nbSlot];
+	conteneur *Cont;
+	int Nanneau; //nombre de conteneur dans l'anneau
+}anneau;
 
 void Ajouter_Event(event e){
 	if(E.taille < MAXEVENT){
@@ -45,8 +70,50 @@ void Init_Echeancier(){
 	Ajouter_Event(e);
 }
 
+int positionStation(int i){
+	return ((150/K)*i);
+}
 
+void Init_Station(anneau *A){
+	int i; 
+	for(i = 0; i<K; i++){
+		A->Stati[i].position = positionStation(i);
+		A->Stati[i].Nstation = 0;
+		A->Stati[i].delta = 0;
+	}
+}
 
+void Init_Slot(anneau *A){
+	int i;
+	for(i = 0; i<nbSlot; i++){
+		A->Slo[i].occupe = 0;
+		A->Slo[i].numero = i;
+	}
+}
+
+void Init_Conteneur(anneau *A){
+	A->Cont = malloc(A->Nanneau * sizeof(conteneur));
+}
+
+void Init_Anneau(anneau *A){
+	Init_Station(A);
+	Init_Slot(A);
+	
+	A->Nanneau = 0;
+	Init_Conteneur(A);
+	
+	
+	//Initialisation variables
+	temps = 0;
+	N = 0;
+	Nservi = 0:
+	
+	
+}
+
+void Ajout_Conteneur(anneau *A){
+	A->Cont = malloc(A->Nanneau * sizeof(conteneur));
+}
 
 
 
